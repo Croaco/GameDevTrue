ff9f9a0a26834a2da2b2764e21c32f31f290ea52
Croaco <59050045+Croaco@users.noreply.github.com>
2021-09-21 12:30:39 +0200

    homework

---
+++ Game/Homework.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <sstream>
+
+
+template <class TYPE> class vec3
+{
+	TYPE x, y, z;
+
+	Zero()
+	{
+		if (x != nullptr && y != nullptr && z != nullptr)
+		{
+			TYPE x, y, z = 0;
+		}
+	}
+
+	isZero(class vec3)
+	{
+		vec3.x
+	}
+
+	vec3::~vec3() //Destructor
+	{
+
+	}
+};
