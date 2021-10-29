cc0bd72c6dc8158be1a64d02ba63e59832998d63
Croaco <abelmontevall@hotmail.com>
2021-10-29 19:24:03 +0200

    More player

--- Game/Source/App.h
+++ Game/Source/App.h
@@ -17,7 +17,7 @@ class Textures;
 class Audio;
 class Scene;
 class Map;
-class Player;
+
 
 class App
 {
@@ -91,7 +91,7 @@ public:
 	Audio* audio;
 	Scene* scene;
 	Map* map;
-	Player* player;
+	
 
 private:
 
--- Game/Source/Player.h
+++ Game/Source/Player.h
@@ -10,7 +10,49 @@
 
 #include "SDL/include/SDL.h"
 
+class Player : public Module 
+{
+public:
 
+	int activelevel = 0;
+
+	iPoint position;
+
+	Animation* currentAnimation = nullptr;
+
+	Animation jumpLeftAnim;
+	Animation jumpRightAnim;
+	Animation leftidleAnim;
+	Animation rightidleAnim;
+	Animation leftAnim;
+	Animation rightAnim;
+
+	Player(bool startEnabled);
+
+	~Player();
+
+	bool Update(float dt);
+	bool Draw();
+	void SetTexture(SDL_Texture* tex);
+	void Jump();
+
+private:
+	SDL_Rect player;
+	SDL_Texture* playerTexture;
+
+	int gravity = 1;
+	int speedx = 1;
+	int speedy = 1;
+	int currenty;
+	int startingy;
+	int jumpingspeedy = 6;
+	float jumpingspeedx = 0.3;
+
+	bool godmode = false;
+
+
+
+};
 
 
 
